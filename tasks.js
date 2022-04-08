f00ac2b04d21f7897b7150bf0f7b68b2d3bc202f
Alina Pakamorytė <99712422+alinapak@users.noreply.github.com>
2022-04-08 11:03:25 +0300

    7 week final task submit

---
+++ tasks.js
@@ -0,0 +1,100 @@
+//1.
+// function biggerOrSmaller (a,b) {
+//    if (a>b) {
+//       console.log(a);
+//    }
+//    else if (b>a) {
+//       console.log(b);
+//    }
+//    else
+//    console.log(b +""+"="+""+ a);
+// }
+// biggerOrSmaller (4,4);
+
+//2.
+// for (let i=1; i<=10;i++) {
+// console.log(i);}
+
+//3.
+// for (let i=0;i<=10;i+=2) {
+//    console.log(i);
+// }
+
+//4.
+// for (let i=0;i<5;i++) {
+//       console.log(Math.floor(Math.random(i)*11));
+//    }
+
+//5.
+// do {
+//    i = (Math.floor(Math.random() * 11));
+//    console.log(i);
+// }
+// while (i !== 5);
+
+//6.
+
+//7. //not done
+// let arr =  [];
+// var res = Math.floor(Math.random() * 101);
+// arr.length = 100;
+// do {
+// if (res > 0 && res <= 25) {
+//    arr.push(res);
+//     console.log("A");
+// }
+// else if (res > 25 && res <= 50) {
+//     console.log("B");
+//     arr.push(res);
+// }
+// else if (res > 50 && res <= 75) {
+//     console.log("C");
+//     arr.push(res);
+// }
+// else {
+// console.log("D");
+// arr.push(res);
+// }
+// }
+// while (arr.length!==100);
+
+//8.  //not done
+// function lygineSuma(a,b) {
+//    let ans = Array.isArray(b);
+//    let ans2 = Array.isArray(a);
+// if (a!==NaN && b!==NaN) {
+//    console.log(a+b);
+// }
+// else if (a!==NaN && b===NaN) {
+//    console.log("error");
+// }
+// else if (b!==NaN && a ===NaN) {
+//    console.log("error");
+// }
+// else if (a===NaN && b===NaN) {
+//    console.log(aArray.length+(bArray.length));
+// }
+// }
+// lygineSuma(5,["aaa"]);
+
+//9. //not done
+// function pirminisSkaicius(x) {
+//    // let pirmsk = 
+//    if (x!==NaN && x%(x/x)!==0) {
+// console.log(x);
+//    }
+//    else 
+//    console.log("Skaicius nera pirminis");
+// }
+// pirminisSkaicius(3);
+
+//10. //not done
+// let z;
+// let arr = [z];
+// function phoneNumber(z) {
+// if (z!==NaN && arr.length===10) {
+// console.log(z)
+// }
+// }
+// phoneNumber(2);
+
